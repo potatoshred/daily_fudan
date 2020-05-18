bbfbfef18b4b26452e99e9a931de5855097dcfcd
K652 <ruslan2kiev@gmail.com>
2020-05-18 11:31:02 +0800

    Update dailyFudan.py

--- dailyFudan.py
+++ dailyFudan.py
@@ -1,38 +1,45 @@
 import requests
-import json, os.path, os, sys
+import json
+import os.path
+import os
+import sys
 from lxml import etree
 from datetime import datetime
 
 
-class zlapp(object):
+class Zlapp:
 
-    def __init__(self):
+    UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0"
+    login_url = r"https://uis.fudan.edu.cn/authserver/login?service=https%3A%2F%2Fzlapp.fudan.edu.cn%2Fa_fudanzlapp%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fzlapp.fudan.edu.cn%252Fsite%252Fncov%252FfudanDaily%253Ffrom%253Dhistory%26from%3Dwap"
+
+    def __init__(self, uid, psw):
         print("start session\n")
-        self.fudanSession = requests.session()
-        self.__login_url = r"https://uis.fudan.edu.cn/authserver/login?service=https%3A%2F%2Fzlapp.fudan.edu.cn%2Fa_fudanzlapp%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fzlapp.fudan.edu.cn%252Fsite%252Fncov%252FfudanDaily%253Ffrom%253Dhistory%26from%3Dwap"
-        self.__cookie = ""
-        self.last_info=""
+        self.zlapp_sess = requests.session()
+        self.zlapp_sess.headers['User-Agent'] = self.UA
+        self.uid = uid
+        self.psw = psw
 
-    def __start(self):
-
-        start = self.fudanSession.get(self.__login_url)
-
+    def _start(self):
+        print("◉Initiating Login Page")
+        start = self.zlapp_sess.get(self.login_url)
         print("return status code", start.status_code)
         if start.status_code == 200:
-            print("◉Initiated login Page")
+            print("◉Initiated")
             return start
         else:
             print("Fail to open Login Page, Check your Internet connection\n")
             self.close()
 
-    def login(self, uid, psw):
-        start = self.__start()
-        print("parsing page")
+    def login(self):
+        start = self._start()
+
+        print("parsing Login page")
         html = etree.HTML(start.text, etree.HTMLParser())
 
+        print("getting tokens")
         parms = {
-            "username": uid,
-            "password": psw,
+            "username": self.uid,
+            "password": self.psw,
             "lt": "",
             "dllt": "userNamePasswordLogin",
             "execution": "",
@@ -41,94 +48,83 @@ class zlapp(object):
             "rmShown": "1"
         }
 
-        print("geting tokens")
         tokens = html.xpath("/html/body/form/input/@value")
         parms["lt"] = tokens[0]
         parms["execution"] = tokens[2]
 
         headers = {
-            "cookies": "",
             "Host": "uis.fudan.edu.cn",
             "Origin": "https://uis.fudan.edu.cn",
             "Referer": "https://uis.fudan.edu.cn/authserver/login?service=https%3A%2F%2Fzlapp.fudan.edu.cn%2Fa_fudanzlapp%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fzlapp.fudan.edu.cn%252Fsite%252Fncov%252FfudanDaily%253Ffrom%253Dhistory%26from%3Dwap",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 Edg/81.0.416.72"
+            "User-Agent": self.UA
         }
 
-        print("generating cookies")
-        cks = dict(start.cookies.items())
-        cks["org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE"] = "en"
-        headers["cookies"] = str(cks)
-
-        print("login ing")
-        post = self.fudanSession.post(
-            self.__login_url, data=parms, headers=headers, allow_redirects=False)
+        print("◉Login ing")
+        post = self.zlapp_sess.post(
+            self.login_url, data=parms, headers=headers, allow_redirects=False)
 
         print("return status code", post.status_code)
+
         if post.status_code == 302:
             print("◉登录成功")
-
         else:
             print("◉登录失败，请检查account.txt中账号信息")
             self.close()
 
-    def get_cks(self):
-        headers = {"cookie":""}
-        k1 = self.fudanSession.get('https://zlapp.fudan.edu.cn/wap/log/save-log',headers=headers).cookies.items()
-        # k2 = self.fudanSession.get('https://zlapp.fudan.edu.cn/ncov/wap/fudan/get-info',headers=headers).cookies.items()
-
-        self.__cookie = str(dict(k1)).replace(": ", "=")[1:-1]
-        # print(self.__cookie)
-
     def check(self):
-        print("检测是否已提交")
-        last_info = self.fudanSession.get('https://zlapp.fudan.edu.cn/ncov/wap/fudan/get-info').text
-        last_info = json.loads(last_info)
-        print("上一次提交日期为", last_info["d"]["info"]["date"])
-        if last_info["d"]["info"]["date"] == self.__date():
-            print("\n\n*******今日已提交*******\n\n")
+        print("◉检测是否已提交")
+        get_info = self.zlapp_sess.get(
+            'https://zlapp.fudan.edu.cn/ncov/wap/fudan/get-info')
+        last_info = json.loads(get_info.text)
+
+        print("◉上一次提交日期为", last_info["d"]["info"]["date"])
+        if last_info["d"]["info"]["date"] == self.date():
+            print("\n\n*******今日已提交*******\n")
             self.close()
 
-        print("*******未提交*******")
+        print("\n\n*******未提交*******")
         self.last_info = last_info["d"]["info"]
 
-
     def checkin(self):
-
         headers = {
             "Host": "zlapp.fudan.edu.cn",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0",
-            "Origin": "https://zlapp.fudan.edu.cn",
-            "Connection": "keep-alive",
             "Referer": "https://zlapp.fudan.edu.cn/site/ncov/fudanDaily?from=history",
-            "Cookie": self.__cookie,
             "DNT": "1",
-            "TE": "Trailers"
+            "TE": "Trailers",
+            "User-Agent": self.UA
         }
 
-        print("◯◯提交中\n\n")
-        self.fudanSession.post(
-            'https://zlapp.fudan.edu.cn/ncov/wap/fudan/save', data=self.last_info, headers=headers)
-        print("********提交成功*******\n********提交成功*******\n********提交成功*******\n\n")
+        print("\n\n◉◉提交中\n\n")
+        save = self.zlapp_sess.post(
+            'https://zlapp.fudan.edu.cn/ncov/wap/fudan/save', data=self.last_info, headers=headers, allow_redirects=False)
 
+        save_msg = json.loads(save.text)["m"]
+
+        if "已 经" in save_msg:
+            print("********提交成功*******")
+            self.close()
+
+        else:
+            print("*****返回信息异常， 检查中*******\n\n")
+            self.check()
 
     def close(self):
-        self.__logout()
-        self.fudanSession.close()
+        self._logout()
+        self.zlapp_sess.close()
         print("\nsession closed")
-
         input("\n\n回车键退出")
         sys.exit(1)
 
-    def __logout(self):
+    def _logout(self):
         exit_url = 'https://uis.fudan.edu.cn/authserver/logout?service=/authserver/login'
-        expire = self.fudanSession.get(exit_url).headers.get('Set-Cookie')
+        expire = self.zlapp_sess.get(exit_url).headers.get('Set-Cookie')
         if '01-Jan-1970' in expire:
             print("\n\n◉登出完毕")
         else:
-            print("\n\nlogout failed")
+            print("\n\n◉登出异常")
 
-    @classmethod
-    def __date(cls):
+    @staticmethod
+    def date():
         now = datetime.now()
         return "%d%02d%02d" % (now.year, now.month, now.day)
 
@@ -157,17 +153,10 @@ if __name__ == "__main__":
         print("账号已保存在目录下account.txt，请注意文件安全，不要放在明显位置\n\n建议从dailyFudan.exe创建快捷方式到桌面")
 
 
-    tmp = zlapp()
 
-    tmp.login(uid, psw)
+    daily_fudan = Zlapp(uid, psw)
+    daily_fudan.login()
+    daily_fudan.check()
+    daily_fudan.checkin()
 
-    tmp.check()
-
-    tmp.get_cks()
-    tmp.checkin()
-
-    tmp.check()
-
-    tmp.close()
-
-
+    daily_fudan.close()
