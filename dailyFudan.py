612f6f2d12dcacdfdb6d2be8ecbb953bd7b4ee63
shiyue2002 <jsclndnz@outlook.com>
2020-11-03 09:43:26 +0800

    .

--- dailyFudan.py
+++ dailyFudan.py
@@ -1,20 +1,20 @@
 from User import User
 import sqlite3 as sql
-import logging
+import logging, sys, getpass
 
 def add_user_data():
-    con = sql.connect("database.db")
+    con = sql.connect(sys.path[0] + "/database.db")
     c = con.cursor()
     uid = input("请输入学号: ")
-    psw = input("请输入密码: ")
-    name = input("请输入姓名（仅用作标识）: ")
+    psw = getpass.getpass("请输入密码（密码不显示）: ")
+    name = input("请输入姓名（仅用作标识，字与字之间请空格）: ")
     data = (uid,psw,name)
     c.execute('insert into user (uid, psw, name) values(?,?,?)',data)
     con.commit()
     con.close()
 
 def print_all_users():
-    con = sql.connect("database.db")
+    con = sql.connect(sys.path[0] + "/database.db")
     c = con.cursor()
     data = c.execute('select * from user;')
     print(data.fetchall())
@@ -22,7 +22,7 @@ def print_all_users():
 
 def dailyFudan():
 
-    con = sql.connect("database.db")
+    con = sql.connect(sys.path[0] + "/database.db")
     c = con.cursor()
     data = c.execute('select * from user')
     result = ""
@@ -41,11 +41,11 @@ def dailyFudan():
                 u.connect.check()
             except RuntimeError as e:
                 logging.warning("User {} failed to submit because of {}".format(u.name,e))
-                result = result + "\nUser {} failed to submit because of {}".format(u.name,e)
+                result = result + "\nFAIL: User {} failed to submit because of {}".format(u.name,e)
                 continue
             u.connect.close()
             logging.warning("User {} submitted successfully".format(u.name))
-            result = result + "\nUser {} submitted successfully".format(u.name)
+            result = result + "\nSUCCESS: User {} submitted successfully".format(u.name)
 
     con.close()
     print(result)
